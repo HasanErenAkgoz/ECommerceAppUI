1113525f07b528b1bf460ab72cf019eb00d024d5
Hasan Eren Akgöz <eren34akgoz@gmail.com>
2023-03-18 21:30:48 +0300

    Added AlertifyJs library to project

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  constructor(private alertifyService : AlertifyService) {
+    
+  }
+  ngOnInit(): void {
+  }
+
+  messageShow(){
+    this.alertifyService.message("Message Showing",{
+      messageType : MessageType.Success,
+      position : Position.BottomLeft
+    });
+
+  }
 
 }
---
+++ src/app/services/admin/alertify.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+declare var alertify : any;
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AlertifyService {
+
+  constructor() { }
+
+  message(message :string,options : Partial<AlertifyOptions>){
+    alertify.set('notifier','position',options.position)
+    alertify[options.messageType](message)
+  }
+}
+
+export class AlertifyOptions{
+  messageType: MessageType = MessageType.Success
+  position : Position = Position.BottomLeft;
+}
+
+export enum MessageType {
+  Error = "error",
+  Message = "message",
+  Notify = "notify",
+  Success = "success",
+  Warning = "warning"
+}
+
+export enum Position{
+  TopCenter = "top-center",
+  TopRight = "top-right",
+  TopLeft = "top-left",
+  BottomRight = "bottom-right",
+  BottomLeft = "bottom-left",
+  BottomCenter = "bottom-center",
+}
